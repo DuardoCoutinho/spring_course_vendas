41ea75c31c756e19c830552eecf6dbb6c7a4c8ec
Eduardo Coutinho <josecoutinho2500@gmail.com>
2023-07-30 23:45:49 -0300

    Aula 12 finalizada

--- src/main/java/io/github/duardocoutinho/vendas/testeConfiguration.java
+++ src/main/java/io/github/duardocoutinho/vendas/testeConfiguration.java
@@ -1,13 +1,19 @@
 package io.github.duardocoutinho.vendas;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
+@Profile("development")
 public class testeConfiguration {
 
-    @Bean(name = "applicationName")
-    public String applicationName() {
-        return "Sistema de Vendas";
+
+    @Bean
+    public CommandLineRunner executar() {
+        return args -> {
+            System.out.println("RODANDO A CONFIGURAÇÃO DE DESENVOLVIMENTO");
+        };
     }
 }
